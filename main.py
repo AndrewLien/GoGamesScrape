79722325e7f0f80bf60b3582d508ca7115126882
andrewdlien1993 <andrew.d.lien.1993@gmail.com>
2020-05-16 19:04:18 +0000

    test selenium webdriver

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import requests
 import json
 import math
-from lxml import etree
-from io import StringIO
-
+import time
+# from lxml import etree
+# from io import StringIO
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 board_size = int(input('What board size do you want? 9 or 13?'))
 assert(board_size == int(9) or board_size == int(13)), 'Not 9 or 13!'
@@ -28,17 +32,35 @@ for x in range(1, 4):  # pagecount+1
       tourney_ids.append(tourney_id)
   print('got page {} of {}'.format(x, pagecount))
 
-def getPage(url):
-    response = StringIO(requests.get(url).text)
-    return etree.parse(response, etree.HTMLParser())
+# def getPage(url):
+#     response = StringIO(requests.get(url).text)
+#     return etree.parse(response, etree.HTMLParser())
 
-for tourney_id in tourney_ids:
-  tree = getPage('https://online-go.com/tournament/{}'.format(tourney_id))
-  '''
-  challenge: page renders with scripts after the page is requested... meaning that we'll need an automated browser to access the content iff I can't find an API
-  '''
-  url = tree.xpath('//a[starts-with(@href, "/game/")]/@href')
-  print(url)
-  
 
+# for tourney_id in tourney_ids:
+#   tree = getPage('https://online-go.com/tournament/{}'.format(tourney_id))
+#   '''
+#   challenge: page renders with scripts after the page is requested... meaning that we'll need an automated browser to access the content iff I can't find an API
+#   '''
+#   url = tree.xpath('//a[starts-with(@href, "/game/")]/@href')
+#   print(url)
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+game_ids = []
+pagetest = open('pagetest.txt', 'w')
+for touney_id in tourney_ids:
+  driver.get('https://online-go.com/tournament/{}'.format(str(tourney_id)))
+  WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID,'//div[@class="top-details"]')))
+  time.sleep(2)
+  x = driver.find_elements_by_xpath('//a[starts-with(@href, "/game/")]/@href')
+  for y in x:
+    game_id = y.split('/')[-1]
+    game_ids.append(game_id)
+    pagetest.write('game_id' + '\n')
+    print('game: {}'.format(game_id))
+
+
+pagetest.close()
 print('Got all tournament links for board size ' + str(board_size))
